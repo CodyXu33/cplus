989dbe1ef40f05784e0904cec50335d48c078016
lapis-hs <13471392@qq.com>
2026-03-18 15:10:28 +0800

    4

---
+++ src/example04.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int age = 20;
+    age = 50;
+
+
+    int32_t  ageInt = 20;
+    cout << "age:" + ageInt << endl;
+ 
+    const int ageConst = 20;
+    return 0;
+}
