5d3befa9a202e83c9c0179ae3af992fe8547b167
lapis-hs <13471392@qq.com>
2026-03-18 15:18:12 +0800

    4

--- src/example04.cpp
+++ src/example04.cpp
@@ -7,7 +7,18 @@ int main(){
 
 
     int32_t  ageInt = 20;
-    cout << "age:" + ageInt << endl;
+    cout << ageInt << endl;
+
+    int a(30);
+    cout << a << endl;
+
+    // auto b = 666;
+    // cout << b << endl;
+
+    double score = 95.5;
+
+    int whole = static_cast<int>(score);
+
  
     const int ageConst = 20;
     return 0;
